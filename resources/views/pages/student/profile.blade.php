05b3dd50c83aaba18ce5555ffce093ab210d3965
afdhal <afdallilo123@gmail.com>
2026-04-27 20:37:37 +0700

    feat: Refactor student profile components into partials for better organization and maintainability

--- resources/views/pages/student/profile.blade.php
+++ resources/views/pages/student/profile.blade.php
@@ -6,6 +6,7 @@
         <h1 class="text-4xl font-bold leading-tight text-slate-900 lg:text-5xl">
             Student Profile
         </h1>
+
         <p class="mt-3 text-lg text-slate-500">
             Manage your personal information and account security.
         </p>
@@ -13,20 +14,20 @@
 
     <div class="grid gap-8 lg:grid-cols-[360px_minmax(0,1fr)]">
         <div class="reveal reveal-delay-1 space-y-6">
-            <x-student.profile-sidebar />
+            @include('partials.student.profile.sidebar')
         </div>
 
         <div class="space-y-8">
             <div class="reveal reveal-delay-1">
-                <x-student.profile-personal-form />
+                @include('partials.student.profile.personal-information')
             </div>
 
             <div class="reveal reveal-delay-2">
-                <x-student.profile-additional-form />
+                @include('partials.student.profile.additional-information')
             </div>
 
             <div class="reveal reveal-delay-3">
-                <x-student.profile-security-form />
+                @include('partials.student.profile.security-password')
             </div>
         </div>
     </div>
